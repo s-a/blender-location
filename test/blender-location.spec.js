a20650ebd188935eb59fe611c26199dc85d6ad27
s-a <stephan.ahlf@gmail.com>
2023-04-03 19:13:10 +0100

    update blender download url

--- test/blender-location.spec.js
+++ test/blender-location.spec.js
@@ -11,6 +11,6 @@ describe('Blender Location', function () {
 		assert.deepStrictEqual(fs.existsSync(res.exe), true, 'could not determine blender executable path')
 		assert.deepStrictEqual(fs.existsSync(res.path.user), true, 'could not determine blender user path')
 		assert.deepStrictEqual(fs.existsSync(res.path.system), true, 'could not determine blender system path')
-		assert.deepStrictEqual(semver.gte(res.version.version, '3.4.0'), true, 'expected blender version to be greater than 3.4.0')
+		assert.deepStrictEqual(semver.gte(res.version.version, '3.5.0'), true, 'expected blender version to be greater than 3.4.0')
 	})
 })
