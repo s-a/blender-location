f838c3484da2953e97935788da6f91f6db340ea9
s-a <stephan.ahlf@gmail.com>
2023-04-03 21:58:13 +0100

    upd timeouot

--- test/blender-location.spec.js
+++ test/blender-location.spec.js
@@ -5,7 +5,7 @@ import semver from 'semver'
 
 describe('Blender Location', function () {
 	it('should return location of blender executable', async function () {
-		this.timeout(4000)
+		this.timeout(20000)
 		const blenderLocation = new BlenderLocation()
 		const res = await blenderLocation.find()
 		assert.deepStrictEqual(semver.gte(res.version.version, '1.0.0'), true, 'expected blender version to be greater than 3.4.0')
